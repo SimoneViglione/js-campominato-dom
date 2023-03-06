eaa7592813cb6560bf29789854671673185bba1d
Simone Viglione <zimoviglio51@gmail.com>
2023-03-06 17:16:33 +0100

    inserito punteggio e alert di game over

--- js/main.js
+++ js/main.js
@@ -4,16 +4,23 @@ const bottoneGriglia = document.getElementById("btn");
 bottoneGriglia.addEventListener("click", () => {
   grid.innerHTML = "";
 
+  let punteggio = 0; 
+
   const numeriBomba = [];
+
   while (numeriBomba.length < 16) {
     const numeroCasuale = Math.floor(Math.random() * 100) + 1;
+
     let giàBomba = false;
+
     for (let i = 0; i < numeriBomba.length; i++) {
+
       if (numeriBomba[i] === numeroCasuale) {
         giàBomba = true;
         break;
       }
     }
+
     if (!giàBomba) {
       numeriBomba.push(numeroCasuale);
     }
@@ -25,6 +32,7 @@ bottoneGriglia.addEventListener("click", () => {
     quadratiGriglia.innerText = i;
 
     let isBomba = false;
+
     for (let j = 0; j < numeriBomba.length; j++) {
       if (numeriBomba[j] === i) {
         isBomba = true;
@@ -35,18 +43,23 @@ bottoneGriglia.addEventListener("click", () => {
     if (isBomba) {
       quadratiGriglia.addEventListener("click", (event) => {
         event.target.classList.add("bomba");
-        alert("BOOM! Hai perso!");
+        alert("BOOM! Hai perso! Punteggio: " + punteggio); 
       });
+      
     } else {
       quadratiGriglia.addEventListener("click", (event) => {
         event.target.classList.toggle("selezionato");
-        console.log(i);
+        punteggio++;
+        if (punteggio === 84) { 
+          alert("Complimenti! Hai vinto! Punteggio: " + punteggio); 
+        }
       });
     }
 
     grid.append(quadratiGriglia);
   }
 });
+
 
 
 
